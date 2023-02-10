2309baaf5e67d62572105e4d0376cb6902bf364a
MariaWaleskaOliver <mwalleska135246@gmail.com>
2023-02-10 13:12:52 -0500

    maria commit

--- CarSell/src/app/components/sign-up/sign-up.component.ts
+++ CarSell/src/app/components/sign-up/sign-up.component.ts
@@ -3,31 +3,20 @@ import { Component ,OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 import { User } from 'src/app/model/signUp.model';
 
-
-
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-  
-  email = new FormGroup({
-    email: new FormControl('',[
-      Validators.required,
-      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    });
-    get primEmail(){
-      return this.email.get('email')
-      }
-  //  loginForm = new FormGroup({
-   // email: new FormControl('',[Validators.required, Validators.email]),
-    //password: new FormControl('')
-  //})  
-  
-  //get email(){return this.loginForm.get('email')}
-  
-  
+    // email = new FormGroup({
+    // email: new FormControl('',[
+    //   Validators.required,
+    //   Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    // });
+    // get primEmail(){
+    //   return this.email.get('email')
+    //   }
   objUser:User = new User();
   
   constructor(){}
