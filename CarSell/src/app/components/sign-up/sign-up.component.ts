d5dd301cb1c44d70629c9f95af67cc7a4b28578d
Sung Ho Park <spark2790@conestogac.on.ca>
2023-02-13 13:33:08 -0500

    Adding sign up API call.

--- CarSell/src/app/components/sign-up/sign-up.component.ts
+++ CarSell/src/app/components/sign-up/sign-up.component.ts
@@ -1,7 +1,9 @@
 import { BuiltinTypeName } from '@angular/compiler';
 import { Component ,OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
-import { User } from 'src/app/model/signUp.model';
+import { UserRegistration } from 'src/app/model/signUp.model';
+import { User} from "../../model/RESTAPI.model";
+import {UsersService} from "../../services/users.service";
 
 @Component({
   selector: 'app-sign-up',
@@ -9,22 +11,39 @@ import { User } from 'src/app/model/signUp.model';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-    // email = new FormGroup({
-    // email: new FormControl('',[
-    //   Validators.required,
-    //   Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
-    // });
-    // get primEmail(){
-    //   return this.email.get('email')
-    //   }
-  objUser:User = new User();
-  
-  constructor(){}
-  ngOnInit(): void{}
+  // email = new FormGroup({
+  // email: new FormControl('',[
+  //   Validators.required,
+  //   Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+  // });
+  // get primEmail(){
+  //   return this.email.get('email')
+  //   }
+  objUser: UserRegistration = new UserRegistration();
 
-  btnSubmit_click(){
-      alert(" Record added");
-    }
+  constructor(private userService: UsersService) {
   }
-   
+
+  ngOnInit(): void {
+  }
+
+  btnSubmit_click() {
+    let apiUser: User = {
+      userId: this.objUser.name,
+      userName: this.objUser.username,
+      email: this.objUser.email,
+      password: this.objUser.password
+    };
+
+    this.userService.createUser(apiUser).subscribe((data) => {
+      if (data.Result == "OK") {
+        // Success code.
+        console.info("Registered successfully.");
+      } else {
+        // Fail code.
+        console.error("Registeration failed.");
+      }
+    });
+  }
+}
 
