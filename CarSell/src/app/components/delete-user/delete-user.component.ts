7b8ce8e7c6d4e4e86a93f604a4eba755c0923e31
Sungho Park <chywoo@gmail.com>
2023-03-01 19:02:41 -0500

    Added showing user information in the delete-user window.

--- CarSell/src/app/components/delete-user/delete-user.component.ts
+++ CarSell/src/app/components/delete-user/delete-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UsersService} from "../../services/users.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {UserIF} from "../../interfaces/restapi.interface";
+import {User} from "../../model/signUp.model";
 
 @Component({
   selector: 'app-delete-user',
@@ -6,11 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-user.component.css']
 })
 export class DeleteUserComponent implements OnInit {
+  userName: string | null = null;
+  objUser: User = new User();
+
+  constructor(private activatedRoute:ActivatedRoute,  private router:Router,private userService: UsersService) {
+  }
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.paramMap.get('id');
+
+    if (this.userName != null) {
+      this.userService.getUserById(this.userName).subscribe((data: UserIF) => {
+        console.info(data);
+        this.objUser.username = data.userName;
+        this.objUser.name = data.fullName;
+        this.objUser.email = data.email;
+        this.objUser.password = data.password;
+      });
+    } else {
+      console.error("Wrong user name.");
+    }
   }
- 
-  btnSubmitWorks(){
-    alert("delete works");
+
+  btnSubmitWorks() {
+
   }
 }
