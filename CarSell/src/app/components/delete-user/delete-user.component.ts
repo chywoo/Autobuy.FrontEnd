192298f0facf04489d5c1edb3e5b1c1d8fc99b7a
Sungho Park <chywoo@gmail.com>
2023-03-01 20:29:59 -0500

    Added deleting user.

--- CarSell/src/app/components/delete-user/delete-user.component.ts
+++ CarSell/src/app/components/delete-user/delete-user.component.ts
@@ -33,6 +33,13 @@ export class DeleteUserComponent implements OnInit {
   }
 
   btnSubmitWorks() {
-
+    // alert(`{id}`);
+    this.userService.deleteUser(this.objUser.username).subscribe((result  ) => {
+      if (result.result == "OK" ) {
+        this.router.navigate(['/userManagement']);
+      } else {
+        alert("Delete user failed.");
+      }
+    });
   }
 }
