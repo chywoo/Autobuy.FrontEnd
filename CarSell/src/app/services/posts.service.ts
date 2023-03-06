ee49c50c20f2e080a93cf5eeac1f3be9a0443a27
Sungho Park <chywoo@gmail.com>
2023-03-06 11:37:09 -0500

    Added the Post Service

--- CarSell/src/app/services/posts.service.ts
+++ CarSell/src/app/services/posts.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import {PostIF, Result} from "../interfaces/restapi.interface";
+
+
+const POSTAPI = "/api/v1/posts";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  /**
+   * Create Post object
+   * @param post
+   */
+  public createPost(post: PostIF): Observable<Result> {
+    return this.http.post<Result>(POSTAPI, post);
+  }
+
+  /**
+   * Get Post List
+   */
+  public getPostList(): Observable<PostIF[]> {
+    return this.http.get<PostIF[]>(POSTAPI);
+  }
+
+  /**
+   * Get Post by Id
+   * @param id
+   */
+  public getPostById(id: string): Observable<PostIF> {
+    return this.http.get<PostIF>(POSTAPI + "/" + id);
+  }
+
+  /**
+   * Update Post by Id
+   */
+  public updatePost(id: string, post: PostIF): Observable<Result> {
+    return this.http.put<Result>(POSTAPI+ "/" + id, post);
+  }
+
+
+  /**
+   * Delete Post by Id
+   */
+  public deletePost(id: string): Observable<Result> {
+    return this.http.delete<Result>(POSTAPI + "/" + id);
+  }
 }
