0cabfa0fa7caad78af9a573e83bb9b98db1c5c8f
Sungho Park <chywoo@gmail.com>
2023-03-06 02:56:45 -0500

    Added more Interfaces.
    for Maker, Car and Post

--- CarSell/src/app/interfaces/restapi.interface.ts
+++ CarSell/src/app/interfaces/restapi.interface.ts
@@ -1,16 +1,41 @@
+export interface  CarIF {
+  carID: number,
+  carName: string,
+  makerID: number,
+  maker: MakerIF
+}
+
+export interface MakerIF {
+  makerID: number,
+  makerName: string
+}
+
 export interface Result {
   result: string,
   message: string
-};
+}
 
 export interface UserIF {
   fullName: string,
   userName: string,
   email: string,
   password: string
-};
+}
 
 export interface LoginIF {
   userName: string,
   password: string
+}
+
+export interface PostIF {
+  postID: number,
+  title: string,
+  description: string,
+  year: number,
+  mileage: number,
+  price: number,
+  userName: string,
+  carID: number,
+  author: UserIF,  // This field is used in the GET method
+  carInfo: CarIF   // This field is used in the GET method
 }
