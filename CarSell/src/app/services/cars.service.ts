ebbd73705fc4f9f544dd6f480412eaa5ed55c481
Sungho Park <chywoo@gmail.com>
2023-03-06 11:33:51 -0500

    Added the Cars APIs.

--- CarSell/src/app/services/cars.service.ts
+++ CarSell/src/app/services/cars.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {Result, CarIF} from "../interfaces/restapi.interface";
+
+const BASEAPI = "/api/v1/cars";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  /**
+   * Get Car list
+   */
+  public getCarList(): Observable<CarIF[]> {
+    let CARAPI = BASEAPI;
+    return this.http.get<CarIF[]>(CARAPI);
+  }
+
+  /**
+   * Get Car by Id
+   */
+  public getMakeInfo(id:number): Observable<CarIF> {
+    let CARAPI = BASEAPI + "/" + id
+    return this.http.get<CarIF>(CARAPI);
+  }
 }
