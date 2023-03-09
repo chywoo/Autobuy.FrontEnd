8445cabb8eca78fca162db3d193b79bb4e5e8e6c
MariaWaleskaOliver <mwalleska135246@gmail.com>
2023-03-09 18:32:05 -0500

    post Seach page fix the cards to be in line

--- CarSell/src/app/components/post-search/post-seach.component.ts
+++ CarSell/src/app/components/post-search/post-seach.component.ts
@@ -7,8 +7,13 @@ import { Component } from '@angular/core';
 })
 export class PostSeachComponent {
   myimage1: string = "assets/img/AlphaRomeo_4C.jpeg";
-  myimage2: string = "assets/img/BMW_3series.jpeg" 
- 
+  myimage2: string = "assets/img/BMW_3series.jpeg" ;
+  myimage3:string = "assets/img/Chevrolet_Corvette.jpeg";
+  myimage4:string = "assets/img/Ford_F150.jpeg";
+  myimage5:string = "assets/img/Genesis_G80.jpeg";
+  myimage6:string = "assets/img/Honda_Civic.jpeg";
+  myimage7:string = "assets/img/Hundai_Tucson.jpeg";
+  myimage8:string = "assets/img/Hyundai_Casper.jpeg";
 
   cards = [
     {
@@ -22,6 +27,42 @@ export class PostSeachComponent {
       description: 'Some quick example text to build on the card title and make up the bulk of the card content',
       buttonText: 'Button',
       img: this.myimage2
+    },
+    {
+      title: 'Card Title 3',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: this.myimage3
+    },
+    {
+      title: 'Card Title 4',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: this.myimage4
+    },
+    {
+      title: 'Card Title 5',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: this.myimage5
+    },
+    {
+      title: 'Card Title 6',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: this.myimage6
+    },
+    {
+      title: 'Card Title 7',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: this.myimage7
+    },
+    {
+      title: 'Card Title 8',
+      description: 'Some quick example text to build on the card title and make up the bulk of the card content',
+      buttonText: 'Button',
+      img: this.myimage8
     },
     
 
