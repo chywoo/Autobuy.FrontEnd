4600c861440c1758d55aa3cec633c0970e73fdc0
Sungho Park <chywoo@gmail.com>
2023-03-10 01:46:23 -0500

    Code reformatting

--- CarSell/src/app/components/add-post/add-post.component.ts
+++ CarSell/src/app/components/add-post/add-post.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Post } from 'src/app/model/post.model';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Post} from 'src/app/model/post.model';
+import {Router} from '@angular/router';
 
 export interface Condition {
   value: string;
@@ -13,18 +13,23 @@ export interface Condition {
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent implements OnInit {
-  objPost:Post = new Post();
-  constructor(private router: Router) {}
+  objPost: Post = new Post();
+
+  constructor(private router: Router) {
+  }
+
   conditions: Condition[] = [
-    {value: 'excellent', viewValue:'1: EXCELLENT'},
+    {value: 'excellent', viewValue: '1: EXCELLENT'},
     {value: 'fine', viewValue: '2: FINE'},
     {value: 'verygood', viewValue: '3: VERY GOOD'},
     {value: 'good', viewValue: '4: GOOD'},
     {value: 'restorable', viewValue: '5: RESTORABLE'},
   ];
-  
-  ngOnInit(): void{}
-  public goToPage(pageName:string) {
+
+  ngOnInit(): void {
+  }
+
+  public goToPage(pageName: string) {
     this.router.navigate([`${pageName}`]);
   }
 }
