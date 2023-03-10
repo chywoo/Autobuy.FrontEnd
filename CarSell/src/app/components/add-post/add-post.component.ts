6b5d339e84dba356b43c261f214d4d8340409f12
Sungho Park <chywoo@gmail.com>
2023-03-10 02:13:34 -0500

    Adding add post

--- CarSell/src/app/components/add-post/add-post.component.ts
+++ CarSell/src/app/components/add-post/add-post.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Post} from 'src/app/model/post.model';
 import {Router} from '@angular/router';
+import {Make} from "../../model/make.model";
+import {MakersService} from "../../services/makers.service";
+import {CarIF} from "../../interfaces/restapi.interface";
+import {CarsService} from "../../services/cars.service";
 
 export interface Condition {
   value: string;
@@ -14,8 +18,10 @@ export interface Condition {
 })
 export class AddPostComponent implements OnInit {
   objPost: Post = new Post();
+  makers: Make[] = [];
+  cars: CarIF[] = [];
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private makersService: MakersService, private carService: CarsService) {
   }
 
   conditions: Condition[] = [
@@ -27,6 +33,33 @@ export class AddPostComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+    this.makersService.getMakerList().subscribe(data => {
+      this.makers = [];
+      for (let i = 0; i < data.length; i++) {
+        let maker = {
+          id: data[i].makerID,
+          name: data[i].makerName
+        }
+        this.makers.push(maker);
+      }
+    });
+
+    // Get all car list
+    this.carService.getCarList().subscribe(data => {
+      this.cars = [];
+
+      for (let i = 0; i < data.length; i++) {
+        let car:CarIF = {
+          carID: data[i].carID,
+          carModel: data[i].carModel,
+          imageURL: data[i].imageURL,
+          makerID: data[i].makerID,
+          maker: data[i].maker,
+        }
+
+        this.cars.push(car);
+      }
+    });
   }
 
   public goToPage(pageName: string) {
