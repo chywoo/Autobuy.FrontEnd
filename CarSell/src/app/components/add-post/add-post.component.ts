a951801c5d42badfcfd7146699247fe4b74994da
Sungho Park <chywoo@gmail.com>
2023-03-13 02:15:14 -0400

    Added creating a new post.

--- CarSell/src/app/components/add-post/add-post.component.ts
+++ CarSell/src/app/components/add-post/add-post.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {Post} from 'src/app/model/post.model';
+import {NewPost, Post} from 'src/app/model/post.model';
 import {Router} from '@angular/router';
 import {Make} from "../../model/make.model";
 import {MakersService} from "../../services/makers.service";
 import {CarIF} from "../../interfaces/restapi.interface";
 import {CarsService} from "../../services/cars.service";
+import {PostsService} from "../../services/posts.service";
+import {AuthService} from "../../services/auth.service";
 
 export interface Condition {
   value: string;
@@ -17,11 +19,18 @@ export interface Condition {
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent implements OnInit {
-  objPost: Post = new Post();
+  objPost: NewPost = new NewPost();
   makers: Make[] = [];
   cars: CarIF[] = [];
 
-  constructor(private router: Router, private makersService: MakersService, private carService: CarsService) {
+  prices: number[] = [5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 50000, 100000, 200000];
+  years: number[] = [...Array(24)].map((_, i) => 2000 + i);
+
+  constructor(private router: Router,
+              private makersService: MakersService,
+              private carService: CarsService,
+              private postsService: PostsService,
+              private authService: AuthService) {
   }
 
   conditions: Condition[] = [
@@ -33,6 +42,27 @@ export class AddPostComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+
+    this.authService.isLoggedIn().subscribe(data => {
+        let value : any = localStorage.getItem('userName');
+
+        // if fail to get userName from localStorage, redirect to login page
+        if (value === null) {
+          alert('You are not logged in. Please login first.');
+          this.router.navigate(['/login']);
+        } else {
+          // set userName to the post object
+          this.objPost.userName = value.toString();
+        }
+      },
+      error => {
+        // if 401 Unauthorized, redirect to login page
+        if (error.status === 401) {
+          alert('You are not logged in. Please login first.');
+          this.router.navigate(['/login']);
+        }
+      });
+
     this.makersService.getMakerList().subscribe(data => {
       this.makers = [];
       for (let i = 0; i < data.length; i++) {
@@ -43,27 +73,27 @@ export class AddPostComponent implements OnInit {
         this.makers.push(maker);
       }
     });
+  }
 
-    // Get all car list
-    this.carService.getCarList().subscribe(data => {
-      this.cars = [];
+  onChange(value: any) {
+    this.cars = [];
 
-      for (let i = 0; i < data.length; i++) {
-        let car:CarIF = {
-          carID: data[i].carID,
-          carModel: data[i].carModel,
-          imageURL: data[i].imageURL,
-          makerID: data[i].makerID,
-          maker: data[i].maker,
-        }
-
-        this.cars.push(car);
+    // get the selected maker's car model list
+    this.carService.getCarListForMake(value.target.value).subscribe(
+      data => {
+        this.cars = data;
       }
+    );
+  }
+
+  public submit(pageName: string) {
+    this.postsService.createPost(this.objPost).subscribe( data => {
+      alert("Post created successfully.")
+
+      this.router.navigate([`"/posts"`]);
+    },
+    error => {
+      alert(error.error.message());
     });
   }
-
-  public goToPage(pageName: string) {
-    this.router.navigate([`${pageName}`]);
-  }
 }
-
