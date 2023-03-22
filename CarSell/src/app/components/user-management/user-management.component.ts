e851a41c1d4e2638c76b30d35a79a689067d71f6
Sungho Park <chywoo@gmail.com>
2023-03-22 01:21:27 -0400

    Added permission check for Users
    "Users" is allowed to only admin.

--- CarSell/src/app/components/user-management/user-management.component.ts
+++ CarSell/src/app/components/user-management/user-management.component.ts
@@ -2,6 +2,8 @@ import { Component,OnInit } from '@angular/core';
 import {UsersService} from "../../services/users.service";
 import { User } from 'src/app/model/signUp.model';
 import { Router } from '@angular/router';
+import {Result} from "../../interfaces/restapi.interface";
+import {AuthService} from "../../services/auth.service";
 
 export interface Element {
   name: string;
@@ -20,10 +22,28 @@ export class UserManagementComponent  implements OnInit{
   displayedColumns: string[] = ['position', 'name','email','username','action'];
   dataSource:Element[] = [] // ELEMENT_DATA;
   objUser:User = new User();
-  constructor(private router:Router,private userService: UsersService) {  }
+  constructor(private router:Router,
+              private userService: UsersService,
+              private authService: AuthService) {  }
 
 
   ngOnInit(): void {
+    // check if user is logged in
+    this.authService.isLoggedIn().subscribe((result: Result) => {
+    }, error => {
+      alert("Login required");
+      this.router.navigate(['/login']);
+      return;
+    });
+
+    let userName = localStorage.getItem("userName");
+    let roleID = localStorage.getItem("roleID");
+
+    if (userName == null || roleID == null) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
     this.userService.getUserList().subscribe((data)=>{
       this.dataSource = [];
       for(let i=0; i<data.length; i++){
@@ -32,6 +52,9 @@ export class UserManagementComponent  implements OnInit{
 
         this.dataSource.push(item);
       }
+    }, error => {
+      alert("You are not authorized to access this page");
+      this.router.navigate(['/']);
     });
   }
 
