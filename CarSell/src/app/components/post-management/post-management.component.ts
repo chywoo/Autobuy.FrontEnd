551e5ee0df1cd61c254a914b6e7d50154ffbdf41
Sungho Park <chywoo@gmail.com>
2023-03-23 23:45:28 -0400

    Moved permission check point from REST API to Angular.
    Users menu has a permission check logic.

--- CarSell/src/app/components/post-management/post-management.component.ts
+++ CarSell/src/app/components/post-management/post-management.component.ts
@@ -41,31 +41,46 @@ export class PostManagementComponent implements OnInit {
 
   ngOnInit(): void {
 
+    // check local storage for user name and role id
+    let userName: any = localStorage.getItem("userName");
+    let roleID: any = localStorage.getItem("roleID");
+
+    if (userName == null || roleID == null) {
+      alert("Login required");
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    // check if user is logged in
     this.authService.isLoggedIn().subscribe((result: Result) => {
+      // only sellers can access this page
+      if (roleID != "2") {
+        alert("You are not authorized to access this page");
+        this.router.navigate(['/']);
+        return;
+      }
+      // if user is logged in, display posts
+      this.displayPosts(userName);
+
     }, error => {
       alert("Login required");
       this.router.navigate(['/login']);
       return;
     });
 
-    let userName = localStorage.getItem("userName");
-    let roleID = localStorage.getItem("roleID");
+  }
 
-    if (userName == null || roleID == null) {
-      this.router.navigate(['/login']);
-      return;
-    }
-
-    this.postsService.getPostList(this.pageSize, this.page, userName).subscribe((data:PostListIF) => {
+  private displayPosts(userName: string) {
+    this.postsService.getPostList(this.pageSize, this.page, userName).subscribe((data: PostListIF) => {
       this.dataSource = []
 
       this.totalPosts = data.total;
-      let posts : PostDetailIF[] = data.posts;
+      let posts: PostDetailIF[] = data.posts;
 
       for (let i = 0; i < posts.length; i++) {
-        let post:Element = {
+        let post: Element = {
           post_id: posts[i].postID,
-          position: i+1,
+          position: i + 1,
           car_make: posts[i].car.make.makeName,
           car_model: posts[i].car.carModel,
           car_price: posts[i].price.toString(),
