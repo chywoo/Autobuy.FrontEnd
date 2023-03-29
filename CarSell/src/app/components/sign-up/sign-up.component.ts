08a5a98e77ee1483d767ba65c35642be1d6155fc
MariaWaleskaOliver <mwalleska135246@gmail.com>
2023-03-29 13:09:52 -0400

    changed the sign up

--- CarSell/src/app/components/sign-up/sign-up.component.ts
+++ CarSell/src/app/components/sign-up/sign-up.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/model/signUp.model';
 import {Result, UserIF} from '../../interfaces/restapi.interface';
 import { Router } from '@angular/router';
 import {UsersService} from "../../services/users.service";
+import {AuthService} from "../../services/auth.service";
 
 
 @Component({
@@ -14,12 +15,34 @@ export class SignUpComponent implements OnInit {
 
   objUser:User = new User();
 
-  constructor(private router:Router, private usersService:UsersService){}
+  constructor(private auth: AuthService, private router:Router, private usersService:UsersService){}
 
-  ngOnInit(): void{}
+  ngOnInit(): void{
+    if (localStorage.getItem('in_progress_login') != 'true') {
+      this.auth.isLoggedIn().subscribe((result: Result) => {
+        if (result.result == "OK") {
+          alert("You need to log out to be able to create another account");
+          this.router.navigate(['/'], {skipLocationChange: true});
+        } else {
+          localStorage.removeItem('userName');
+          localStorage.removeItem('fullName');
+          localStorage.removeItem('roleID');
+          localStorage.removeItem('roleName');
+          localStorage.removeItem('in_progress_login');
+        }
+      }, (error) => {
+        //console.error(error);
+      });
+    } else {
+      localStorage.removeItem('in_progress_login');
+    }
+  }
+  
+
+  
 
   public goToPage(pageName:string):void {
-    // @ts-ignore
+    
     let user : UserIF = {
       fullName: this.objUser.name,
       userName: this.objUser.username,
@@ -36,6 +59,7 @@ export class SignUpComponent implements OnInit {
         console.info("Success");
         this.router.navigate([`${pageName}`]);
       } else {
+        alert(data.message)
         console.error(data.message);
       }
     });
